377b5654bbc99dcf270d172abcfedcb89f58e90f
dinesahu <dinesahu@AMGENEXTWEB.COM>
2019-08-23 19:26:36 +0530

    mvn

---
+++ src/main/java/helloworldApp/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworldApp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my first app to maven and jenkins ");
+
+	}
+
+}
